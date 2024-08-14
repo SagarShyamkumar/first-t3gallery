3fe2ce75e01ccf5ca497314082e148887c8bf07f
Sagar Shyamkumar <Sagar07shyam@gmail.com>
2024-08-14 18:43:29 +0530

    Scaffold UI

--- README.md
+++ README.md
@@ -1,14 +1,13 @@
-#T3 Gallery
-##TO DO
-- [ ]Make it deploy
-- [ ]Scaffold basic ui with mock data
-- [ ]Actually set up a database(vercel Postgres)
-- [ ]Attach database to ui
-- [ ]Add Authentication
-- [ ]Add image upload
-- [ ]Error management(w/ sentry)
-- [ ]Routing image/image (parallel route)
-- [ ]Delete buttons(w/ Server Actions)
-- [ ]Analytics(Posthog)
-- [ ]Ratelimiting(upstash)
-- [ ]
+# T3 Gallery
+## TODO
+- [x] Made it deploy (vercel)
+- [x] Scaffold basic ui with mock data
+- [ ] Actually set up a database (vercel postgres)
+- [ ] Attach database to UI
+- [ ] Add authentication (w/ clerk)
+- [ ] Add image upload
+- [ ] Error management (w/ Sentry)
+- [ ] Routing/image page (parallel route)
+- [ ] Delete button (w/ Server Action)
+- [ ] Analytics (posthog)
+- [ ] Rarelimiting (upstash)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,28 @@
 import Link from "next/link";
+const mockUrls = [
+  "https://utfs.io/f/213a3d1f-d67d-470c-98b2-0470034b21be-1d.jfif",
+  "https://utfs.io/f/8593af2f-fb2f-40fe-bf30-3d3689d78db3-1e.jfif",
+  "https://utfs.io/f/b9caa771-8fe9-4294-87e4-a5cc331c248e-1f.jfif",
+  "https://utfs.io/f/4e213c02-067e-430c-80bd-5aebfd782743-1g.jfif"
+];
+
+const mockimages = mockUrls.map((url,index) => ({
+  id: index + 1,
+  url,
+}));
+
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello(gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockimages, ...mockimages, ...mockimages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} />
+          </div>
+        ))
+        }
+      </div>
     </main>
   );
 }
