5b407c3f8d3899813881265a41d388906540da4f
Sagar Shyamkumar <Sagar07shyam@gmail.com>
2024-09-23 12:59:21 +0530

    Basic Posthog analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { extractRouterConfig } from "uploadthing/server";
 import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
 import { ourFileRouter } from "./api/uploadthing/core";
 import { Toaster } from "sonner";
+import { CSPostHogProvider } from "./_analytics/provider";
 
 export const metadata: Metadata = {
   title: "T3 Gallery",
@@ -27,20 +28,22 @@ export default function RootLayout({
   {
   return (
     <ClerkProvider>
-     <html lang="en" >
-     <NextSSRPlugin
-          routerConfig={extractRouterConfig(ourFileRouter)}
-        />
-        <body className={`${GeistSans.variable}dark`}>
-        <div className="grid h-screen grid-rows-[auto,1fr]">
-          <TopNav />
-          <main className="overflow-y-scroll">{children}</main>
-        </div>
-        {modal}
-        <div id="modal-root" />
-        <Toaster />
-       </body>
-      </html>
+      <CSPostHogProvider>  
+        <html lang="en" >
+        <NextSSRPlugin
+              routerConfig={extractRouterConfig(ourFileRouter)}
+            />
+            <body className={`${GeistSans.variable}dark`}>
+            <div className="grid h-screen grid-rows-[auto,1fr]">
+              <TopNav />
+              <main className="overflow-y-scroll">{children}</main>
+            </div>
+            {modal}
+            <div id="modal-root" />
+            <Toaster />
+          </body>
+          </html>
+        </CSPostHogProvider>
     </ClerkProvider>
   );
 }
